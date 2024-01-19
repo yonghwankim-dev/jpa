aec1de04fed194f52940e21a82201468da1b43fa
yonghwankim-dev <dragonbead95@naver.com>
2024-01-19 11:48:55 +0900

    code clean

--- src/main/java/com/ch14/class04/step03/OrderService.java
+++ src/main/java/com/ch14/class04/step03/OrderService.java
@@ -8,11 +8,7 @@ import javax.persistence.EntityGraph;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Subgraph;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
+import javax.transaction.Transactional;
 
 @Service
 public class OrderService {
